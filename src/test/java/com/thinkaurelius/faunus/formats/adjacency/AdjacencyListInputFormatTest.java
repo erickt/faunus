e9488969c53167d9128783ed898fbad8c2586f46
Charlie Lewis <clewis@iqt.org>
2014-01-16 22:50:39 +0000

    added tests for adjacency

---
+++ src/test/java/com/thinkaurelius/faunus/formats/adjacency/AdjacencyListInputFormatTest.java
@@ -0,0 +1,14 @@
+package com.thinkaurelius.faunus.formats.adjacency; 
+
+import com.thinkaurelius.faunus.tinkerpop.gremlin.Imports;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AdjacencyListInputFormatTest {
+
+    @Test
+    public void testInGremlinImports() {
+        assertTrue(Imports.getImports().contains(AdjacencyFileInputFormat.class.getPackage().getName() + ".*"));
+    }
+}
